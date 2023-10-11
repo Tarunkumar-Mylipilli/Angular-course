6d8d33e7838e175574dd52d7815d9175da7b16cc
Tarunkumar-Mylipilli <tarun.developer99@gmail.com>
2023-10-11 19:30:39 +0530

    updation of input decorator

--- MyApp/src/app/app.module.ts
+++ MyApp/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TemplatereferenceVariableComponent } from './templatereference-variable/templatereference-variable.component';
 import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { HeaderComponent } from './header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
